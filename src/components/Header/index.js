2437ca5e9ad0274d21934695f4e6d1d942e131e3
nicolas.ruiz <nicolas.ruiz@cdbdx.biz>
2018-03-09 16:24:45 +0100

    fix get articles in cart

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,63 +1,64 @@
-import React, { PureComponent } from 'react';
-import { Menu } from 'semantic-ui-react';
-import { Link } from 'react-router-dom';
-import './style.css';
+import React, { PureComponent } from "react";
+import { Menu } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import "./style.css";
+
 class Header extends PureComponent {
-	constructor(props) {
-		super(props);
-		this.state = { activeItem: '' };
-	}
+  constructor(props) {
+    super(props);
+    this.state = { activeItem: "" };
+  }
 
-	handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
 
-	render() {
-		const { activeItem } = this.state;
-		return (
-			<Menu className="custom-header">
-				<Menu.Menu position="right">
-					<Menu.Item
-						as={Link}
-						to="/"
-						name="accueil"
-						active={activeItem === 'accueil'}
-						onClick={this.handleItemClick}
-						className="uppercase-header-item"
-					/>
+  render() {
+    const { activeItem } = this.state;
+    return (
+      <Menu className="custom-header">
+        <Menu.Menu position="right">
+          <Menu.Item
+            as={Link}
+            to="/"
+            name="accueil"
+            active={activeItem === "accueil"}
+            onClick={this.handleItemClick}
+            className="uppercase-header-item"
+          />
 
-					<Menu.Item
-						name="boutique"
-						as={Link}
-						to="/articles"
-						active={activeItem === 'boutique'}
-						onClick={this.handleItemClick}
-						className="uppercase-header-item"
-					/>
-					<Menu.Item
-						name="panier"
-						as={Link}
-						to="/cart"
-						active={activeItem === 'cart'}
-						onClick={this.handleItemClick}
-						className="uppercase-header-item"
-					/>
-				</Menu.Menu>
-				<Menu.Menu position="right">
-					<Menu.Item
-						name="contact"
-						active={activeItem === 'contact'}
-						onClick={this.handleItemClick}
-						className="uppercase-header-item"
-					/>
-					<Menu.Item
-						name="à propos"
-						active={activeItem === 'about'}
-						onClick={this.handleItemClick}
-						className="uppercase-header-item"
-					/>
-				</Menu.Menu>
-			</Menu>
-		);
-	}
+          <Menu.Item
+            name="boutique"
+            as={Link}
+            to="/articles"
+            active={activeItem === "boutique"}
+            onClick={this.handleItemClick}
+            className="uppercase-header-item"
+          />
+          <Menu.Item
+            name="panier"
+            as={Link}
+            to="/cart"
+            active={activeItem === "cart"}
+            onClick={this.handleItemClick}
+            className="uppercase-header-item"
+          />
+        </Menu.Menu>
+        <Menu.Menu position="right">
+          <Menu.Item
+            name="contact"
+            active={activeItem === "contact"}
+            onClick={this.handleItemClick}
+            className="uppercase-header-item"
+          />
+          <Menu.Item
+            name="à propos"
+            active={activeItem === "about"}
+            onClick={this.handleItemClick}
+            className="uppercase-header-item"
+          />
+        </Menu.Menu>
+      </Menu>
+    );
+  }
 }
 
 export default Header;
--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,17 +1,19 @@
-import React, { Component } from 'react';
-import Header from '../Header';
-import { Container } from 'semantic-ui-react';
-import './style.css';
+import React, { Component } from "react";
+import Header from "../Header";
+import { Container } from "semantic-ui-react";
+import "./style.css";
+
 class Page extends Component {
-	render() {
-		return (
-			<div>
-				<Header />
-				
-				<Container className={this.props.isFluid ? 'fluid' : ''}>{this.props.children}</Container>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <Header />
+        <Container className={this.props.isFluid ? "fluid" : ""}>
+          {this.props.children}
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default Page;
